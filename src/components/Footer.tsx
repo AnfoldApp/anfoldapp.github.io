6439e1c963da4c07a7235dd8593f25f561acf28d
Vlad <vlad@cazacu.me>
2025-08-02 19:36:55 +0200

    fixed responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,32 +3,33 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="footer text-sm">
+    <footer className="footer text-sm flex flex-col lg:flex-row items-center justify-between gap-4">
       <span>© 2025 Avocado Technologies UG (haftungsbeschränkt)</span>
 
-      <a
-        href="https://discord.gg/mrk8HQmJ"
-        target="_blank"
-        title="Help shape the product on Discord"
-        className="ml-md-auto"
-      >
-        <Image
-          src="/assets/images/discord.svg"
-          alt="Discord"
-          className="icon"
-          width={40}
-          height={40}
-        />
-      </a>
-      <Link href="/terms-of-service" className="with-underline">
-        Terms of Service
-      </Link>
-      <Link href="/privacy-policy" className="with-underline">
-        Privacy Policy
-      </Link>
-      <Link href="/imprint" className="with-underline">
-        Imprint
-      </Link>
+      <div className="flex flex-col lg:flex-row items-center gap-4">
+        <Link
+          href="https://discord.gg/mrk8HQmJ"
+          target="_blank"
+          title="Help shape the product on Discord"
+        >
+          <Image
+            src="/assets/images/discord.svg"
+            alt="Discord"
+            className="icon"
+            width={40}
+            height={40}
+          />
+        </Link>
+        <Link href="/terms-of-service" className="with-underline">
+          Terms of Service
+        </Link>
+        <Link href="/privacy-policy" className="with-underline">
+          Privacy Policy
+        </Link>
+        <Link href="/imprint" className="with-underline">
+          Imprint
+        </Link>
+      </div>
     </footer>
   );
 }
